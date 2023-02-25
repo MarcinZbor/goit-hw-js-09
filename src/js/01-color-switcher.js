212dfd6714b663230a614bdb209718946a98e007
MarcinZbor <marcinzb@gazeta.pl>
2023-02-25 09:42:39 +0100

    1zadanie

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
-const startBtn = document.querySelector('button');
+const startBtn = document.querySelector('button[data-start]');
+// console.log(startBtn);
+const stopBtn = document.querySelector('button[data-stop]');
+// console.log(stopBtn);
+const bodyColor = document.querySelector('body');
+// console.log(bodyColor);
+let timeId = null;
+
+startBtn.addEventListener('click', () => {
+  timeId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.setAttribute('disabled', 'disabled');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timeId);
+
+  startBtn.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
