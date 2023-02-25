3617c95c46aeb11013819fa0290965be2f1a1767
MarcinZbor <113549466+MarcinZbor@users.noreply.github.com>
2023-02-25 08:43:01 +0000

    Deploying to gh-pages from @ MarcinZbor/goit-hw-js-09@212dfd6714b663230a614bdb209718946a98e007 🚀

--- 01-color-switcher.a55b8318.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button");
-//# sourceMappingURL=01-color-switcher.a55b8318.js.map
--- 01-color-switcher.f8d9a972.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button");
-//# sourceMappingURL=01-color-switcher.f8d9a972.js.map
